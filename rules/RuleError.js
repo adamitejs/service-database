9f6cc499bf1adcc6e60c2a3e47a8b906770d425d
Jesse Dunlap <jesse@Jesses-iMac.local>
2019-04-26 21:20:32 -0500

    Added rule validation

---
+++ rules/RuleError.js
@@ -0,0 +1,8 @@
+class RuleError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+module.exports = RuleError;
--- src/ArcDatabase.js
+++ src/ArcDatabase.js
@@ -1,4 +1,5 @@
 const { server } = require('@arc/relay');
+const { RuleValidator } = require('../rules');
 const DatabaseDeserializer = require('@arc/sdk/core/serialization/DatabaseDeserializer');
 const uuid = require('uuid');
 const rethinkdb = require('../adapters/rethinkdb');
@@ -7,6 +8,7 @@ class ArcDatabase {
   constructor(config) {
     this.config = config;
     this.server = server({ apiUrl: 'http://localhost:9000', port: 9001 });
+    this.rules = new RuleValidator(this.config.database.rules);
     this.adapter = rethinkdb(this.config);
     this.adapter.connect();
     this.registerCommands();
@@ -18,6 +20,7 @@ class ArcDatabase {
       
       try {
         const data = await this.adapter.readDocument(ref);
+        await this.rules.validateRule('read', ref, { ref }, { data });
         callback({ error: false, snapshot: { ref, data } });
       } catch (err) {
         console.error(err);
@@ -29,6 +32,7 @@ class ArcDatabase {
       const ref = DatabaseDeserializer.deserializeCollectionReference(args.ref);
       
       try {
+        await this.rules.validateRule('create', ref, { ref });
         const data = await this.adapter.createDocument(ref, args.data);
         const documentRef = ref.doc(data.id);
         callback({ error: false, snapshot: { ref: documentRef, data } });
@@ -42,6 +46,7 @@ class ArcDatabase {
       const ref = DatabaseDeserializer.deserializeDocumentReference(args.ref);
       
       try {
+        await this.rules.validateRule('update', ref, { ref });
         const data = await this.adapter.updateDocument(ref, args.data);
         callback({ error: false, snapshot: { ref, data } });
       } catch (err) {
@@ -54,6 +59,7 @@ class ArcDatabase {
       const ref = DatabaseDeserializer.deserializeDocumentReference(args.ref);
       
       try {
+        await this.rules.validateRule('delete', ref, { ref });
         await this.adapter.deleteDocument(ref, args.data);
         callback({ error: false, snapshot: { ref } });
       } catch (err) {
@@ -68,18 +74,24 @@ class ArcDatabase {
       try {
         const id = uuid.v4();
         
-        await this.adapter.subscribeDocument(ref, (err, oldData, newData) => {
+        await this.adapter.subscribeDocument(ref, async (err, oldData, newData) => {
           if (err) {
             console.error(err);
             return this.client.socket.emit(id, { error: err.message, subscription: { ref, id } });
           }
 
-          client.socket.emit(id, {
-            error: false,
-            subscription: { ref, id },
-            newSnapshot: newData && { ref, data: newData },
-            oldSnapshot: oldData && { ref, data: oldData },
-          });
+          try {
+            await this.rules.validateRule('read', ref, { ref }, { data: newData });
+
+            client.socket.emit(id, {
+              error: false,
+              subscription: { ref, id },
+              newSnapshot: newData && { ref, data: newData },
+              oldSnapshot: oldData && { ref, data: oldData },
+            });
+          } catch (err) {
+            console.error(err);
+          }
         });
 
         callback({ error: false, subscription: { ref, id } });
@@ -95,18 +107,37 @@ class ArcDatabase {
       try {
         const id = uuid.v4();
         
-        await this.adapter.subscribeCollection(ref, (err, oldData, newData) => {
+        await this.adapter.subscribeCollection(ref, async (err, oldData, newData) => {
           if (err) {
             console.error(err);
             return this.client.socket.emit(id, { error: err.message, subscription: { ref, id } });
           }
 
-          client.socket.emit(id, {
-            error: false,
-            subscription: { ref, id },
-            newSnapshot: newData && { ref: ref.doc(newData.id), data: newData },
-            oldSnapshot: oldData && { ref: ref.doc(oldData.id), data: oldData },
-          });
+          const newDocRef = newData && ref.doc(newData.id);
+          const oldDocRef = oldData && ref.doc(oldData.id);
+
+          try {            
+            if (newDocRef) {
+              await this.rules.validateRule('read', newDocRef, { newDocRef }, { data: newData });
+            }
+
+            client.socket.emit(id, {
+              error: false,
+              subscription: { ref, id },
+              newSnapshot: newData && { ref: newDocRef, data: newData },
+              oldSnapshot: oldData && { ref: oldDocRef, data: oldData },
+            });
+          } catch (err) {
+            console.error(err);
+
+            if (oldData) {
+              client.socket.emit(id, {
+                error: false,
+                subscription: { ref, id },
+                oldSnapshot: oldData && { ref: oldDocRef, data: oldData },
+              });
+            }
+          }
         });
 
         callback({ error: false, subscription: { ref, id } });

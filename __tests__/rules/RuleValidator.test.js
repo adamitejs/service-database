cbb670aa8f102443807d424cdded7139a7b1aaba
Prozacgod <prozacgod@gmail.com>
2019-06-30 15:21:53 -0500

    Changed tests to cover targeted refactor

--- __tests__/rules/RuleValidator.test.js
+++ __tests__/rules/RuleValidator.test.js
@@ -1,8 +1,24 @@
-const RuleValidator = require("../../rules/RuleValidator");
-const RuleError = require("../../rules/RuleError");
+const { RuleValidator, RuleError } = require("../../rules");
 
 const { DocumentReference, CollectionReference } = require("@adamite/sdk");
 
+function getterTrackingProxy(objName) {
+  const gets = [];
+  const _getterTrackingProxy = objName =>
+    new Proxy(
+      {},
+      {
+        get: function(target, name) {
+          gets.push(`${objName}.${name}`);
+          return _getterTrackingProxy(`${objName}.${name}`);
+        }
+      }
+    );
+
+  return [gets, _getterTrackingProxy(objName)];
+}
+
+/*TODO: hack mocks - could be done better */
 function MockDocumentReference(databaseName, collectionName) {
   this.collection = {
     name: collectionName,
@@ -22,8 +38,96 @@ function MockCollectionReference(databaseName, collectionName) {
 MockCollectionReference.prototype = CollectionReference.prototype;
 
 describe("RuleValidator", () => {
+  let ruleValidator;
+  let doc;
+  let col;
+
+  beforeEach(() => {
+    ruleValidator = new RuleValidator({});
+    doc = new MockDocumentReference();
+    col = new MockCollectionReference();
+  });
+
   it("Can be constructed", () => {
     const x = new RuleValidator(null);
+  });
+
+  describe("getRuleForRef", () => {
+    beforeEach(() => {
+      ruleValidator.getDocumentRule = jest.fn();
+      ruleValidator.getCollectionRule = jest.fn();
+    });
+
+    it("returns undefined for unknown refs", () => {
+      expect(ruleValidator.getRuleForRef({}, "tickle")).toBe(undefined);
+      expect(ruleValidator.getDocumentRule.mock.calls.length).toBe(0);
+      expect(ruleValidator.getCollectionRule.mock.calls.length).toBe(0);
+    });
+
+    it("can get document rules for document refs", () => {
+      expect(ruleValidator.getRuleForRef(doc, "tickle")).toBe(undefined);
+      expect(ruleValidator.getDocumentRule.mock.calls.length).toBe(1);
+      expect(ruleValidator.getCollectionRule.mock.calls.length).toBe(0);
+    });
+
+    it("can get collection rules for collection refs", () => {
+      expect(ruleValidator.getRuleForRef(col, "tickle")).toBe(undefined);
+      expect(ruleValidator.getDocumentRule.mock.calls.length).toBe(0);
+      expect(ruleValidator.getCollectionRule.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe("getXXXRule", () => {
+    beforeEach(() => {
+      ruleValidator.getRuleForDbCollection = jest.fn();
+    });
+
+    it("getDocumentRule traverses the correct object properties", () => {
+      const [lookups, proxyDoc] = getterTrackingProxy("doc");
+      ruleValidator.getDocumentRule(proxyDoc, "tickle");
+      expect(lookups).toEqual([
+        "doc.collection",
+        "doc.collection.database",
+        "doc.collection.database.name",
+        "doc.collection",
+        "doc.collection.name"
+      ]);
+    });
+
+    it("getCollectionRule traverses the correct object properties", () => {
+      const [lookups, proxyDoc] = getterTrackingProxy("doc");
+      ruleValidator.getCollectionRule(proxyDoc, "tickle");
+      expect(lookups).toEqual(["doc.database", "doc.database.name", "doc.name"]);
+    });
+  });
+
+  describe("getRuleForDbCollection", () => {
+    const mock = {
+      getRuleForDbCollection: RuleValidator.prototype.getRuleForDbCollection
+    };
+
+    // no rules
+    expect(() => mock.getRuleForDbCollection("foo", "bar", "tickle")).toThrow();
+
+    // empty rules
+    mock.rules = {};
+    expect(mock.getRuleForDbCollection("foo", "bar", "tickle")).toBe(undefined);
+
+    // wrong database
+    mock.rules = { foo: {} };
+    expect(mock.getRuleForDbCollection("fizz", "bar", "tickle")).toBe(undefined);
+
+    // right database - missing collection
+    mock.rules = { foo: { bar: {} } };
+    expect(mock.getRuleForDbCollection("foo", "fizz", "tickle")).toBe(undefined);
+
+    // right database - correct collection - no operation
+    mock.rules = { foo: { bar: {} } };
+    expect(mock.getRuleForDbCollection("foo", "bar", "tickle")).toBe(undefined);
+
+    // right database - correct collection - correct operation
+    mock.rules = { foo: { bar: { tickle: 42 } } };
+    expect(mock.getRuleForDbCollection("foo", "bar", "tickle")).toBe(42);
   });
 
   describe("validateRule", () => {
@@ -51,72 +155,6 @@ describe("RuleValidator", () => {
       const x = new RuleValidator({ foo: {} });
 
       expect(x.validateRule("tickle", doc, {}, {})).resolves.toEqual(undefined);
-    });
-  });
-
-  describe("getRuleForRef", () => {
-    it("returns undefined if ref is not Document or Collection.", () => {
-      const x = new RuleValidator({});
-
-      const result = x.getRuleForRef({}, "");
-
-      expect(result).toBe(undefined);
-    });
-
-    describe("with Document ref", () => {
-      it("returns undefined if db name IS NOT found.", () => {
-        const doc = new MockDocumentReference("foo", "bar");
-        const x = new RuleValidator({});
-        const result = x.getRuleForRef(doc, "");
-        expect(result).toBe(undefined);
-      });
-
-      it("returns undefined if db name IS found, and collection IS NOT found.", () => {
-        const doc = new MockDocumentReference("foo", "bar");
-        const x = new RuleValidator({ foo: {} });
-        const result = x.getRuleForRef(doc, "");
-        expect(result).toBe(undefined);
-      });
-
-      it("returns operation rule if db name IS found, and collection IS found.", () => {
-        const doc = new MockDocumentReference("foo", "bar");
-
-        const x1 = new RuleValidator({ foo: { bar: {} } });
-        const result1 = x1.getRuleForRef(doc, "tickle");
-        expect(result1).toBe(undefined);
-
-        const x2 = new RuleValidator({ foo: { bar: { tickle: 42 } } });
-        const result2 = x2.getRuleForRef(doc, "tickle");
-        expect(result2).toBe(42);
-      });
-    });
-
-    describe("with Collection ref", () => {
-      it("returns undefined if db name IS NOT found.", () => {
-        const col = new MockCollectionReference("foo", "bar");
-        const x = new RuleValidator({});
-        const result = x.getRuleForRef(col, "");
-        expect(result).toBe(undefined);
-      });
-
-      it("returns undefined if db name IS found, and collection IS NOT found.", () => {
-        const col = new MockCollectionReference("foo", "bar");
-        const x = new RuleValidator({ foo: {} });
-        const result = x.getRuleForRef(col, "");
-        expect(result).toBe(undefined);
-      });
-
-      it("returns operation rule if db name IS found, and collection IS found.", () => {
-        const col = new MockCollectionReference("foo", "bar");
-
-        const x1 = new RuleValidator({ foo: { bar: {} } });
-        const result1 = x1.getRuleForRef(col, "tickle");
-        expect(result1).toBe(undefined);
-
-        const x2 = new RuleValidator({ foo: { bar: { tickle: 42 } } });
-        const result2 = x2.getRuleForRef(col, "tickle");
-        expect(result2).toBe(42);
-      });
     });
   });
 });
